3ac9a439ba015ea5822ff60d8d69352dd4df0127
Luke Hudson <lukeletters@gmail.com>
2013-09-14 15:08:33 +0200

    Working thumb size controls

--- main.js
+++ main.js
@@ -1,11 +1,35 @@
 global.$ = $;
 
 var abar = require('address_bar');
+var controls = require('controls');
 var folder_view = require('folder_view');
 var path = require('path');
 var gui = require('nw.gui')
 var fs = require('fs');
 var shell = gui.Shell;
+
+function getRuleBySelector(sheet, selector) {
+  var rules = sheet.cssRules, rule;
+  for(var i=0; i < rules.length; i++) {
+    rule = rules[i];
+    if (rule.selectorText === selector) {
+      return rule;
+    }
+  }
+  return false;
+}
+
+function getCSSRule(rule) {
+  var SS = document.styleSheets, sheet, match;
+  for(var i=0; i < SS.length; i++) {
+    sheet = SS[i];
+    match = getRuleBySelector(sheet, rule);
+    if (match !== false) {
+      return match;
+    }
+  }
+  return false;
+}
 
 $(document).ready(function() {
   var app = gui.App;
@@ -21,6 +45,16 @@ $(document).ready(function() {
 
   var folder = new folder_view.Folder(gui.App, $('#files'));
   var addressbar = new abar.AddressBar($('#addressbar'));
+
+  var thumbSize = new controls.ThumbSize(gui.App, $('#thumb-size-input'));
+
+  thumbSize.on('update', function(thumbSize) {
+    console.log("New thumbnail size: ", thumbSize);
+    var cssRule = getCSSRule('.file');
+    if (cssRule) {
+      cssRule.style.width = thumbSize + 'px';
+    }
+  });
 
   folder.open(startDir);
   addressbar.set(startDir);
